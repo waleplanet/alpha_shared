6dadf0aa66a30a8cc61408b5f60f36362ebd1729
waleplanet <walexrealest@gmail.com>
2019-09-11 12:34:03 +0100

    update user created event struct

--- events/contracts.go
+++ events/contracts.go
@@ -2,7 +2,8 @@ package events
 
 type UserCreatedEvent struct {
 	ID    string `json:"id"`
-	Email string `json:email`
+	Email string `json:"email""`
+	Token string `json:"token"`
 }
 
 func (e *UserCreatedEvent) EventName() string {
