cb3b7ba526c18c3dc25930755273f5d2d29288af
waleplanet <walexrealest@gmail.com>
2019-10-08 20:03:04 +0100

    update wallet_no to string

--- models/model.go
+++ models/model.go
@@ -10,7 +10,7 @@ type Wallet struct {
 	LedgerBal      float64 `json:"ledger_balance"`
 	AccountType    string  `gorm:"type:varchar(1);not null" json:"account_type"`
 	WalletID       int64   `gorm:"size:255;not null" json:"id"`
-	WalletNo       int64   `gorm:"size:255;not null" json:"wallet_number"`
+	WalletNo       string  `gorm:"size:255;not null" json:"wallet_number"`
 	Currency       string  `gorm:"type:varchar(20);not null" json:"currency"`
 	Status         string  `json:"status"`
 	DateCreated    string  `json:"date_created"`
