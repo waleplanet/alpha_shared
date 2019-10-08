fde4ff4983c910af9350dee1242c91669556dbd7
waleplanet <walexrealest@gmail.com>
2019-10-08 13:35:20 +0100

    add json annotation to wallet model

--- models/model.go
+++ models/model.go
@@ -5,13 +5,13 @@ import "github.com/jinzhu/gorm"
 type Wallet struct {
 	//"id": 138,
 	gorm.Model
-	CustomerID     int64 `gorm:"size:255;not null;unique"`
-	AvailableBal   float64
-	LedgerBal      float64
-	AccountType    string
-	WalletNo       string
-	Currency       string
-	Status         string
-	DateCreated    string
-	DateBalUpdated string
+	CustomerID     int64   `gorm:"size:255;not null;unique"`
+	AvailableBal   float64 `json:"available_balance"`
+	LedgerBal      float64 `json:"ledger_balance"`
+	AccountType    string  `gorm:"type:varchar(1);not null",json:"account_type"`
+	WalletNo       string  `gorm:"type:varchar(20);not null",json:"wallet_number"`
+	Currency       string  `gorm:"type:varchar(20);not null",json:"currency"`
+	Status         string  `json:"status"`
+	DateCreated    string  `json:"date_created"`
+	DateBalUpdated string  `json:"date_balance_updated"`
 }
