aa316603ef944a8a0975137a8e9151395ed0a970
waleplanet <walexrealest@gmail.com>
2019-10-12 13:05:10 +0100

    add rand helper

---
+++ helper/rand.go
@@ -0,0 +1,41 @@
+package helper
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"log"
+	"math/rand"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var seedRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+const charset = "abcdefghijklmnopqrstuvwxyz" +
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func RandStringWithCharSet(length int, charset string) string {
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seedRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandString(length int) string {
+	return RandStringWithCharSet(length, charset)
+}
+
+func GenerateToken(word string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(word), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hasher := md5.New()
+	hasher.Write(hash)
+	return hex.EncodeToString(hasher.Sum(nil))
+}
