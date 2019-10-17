f8d75f5658afaa6867675359179a5e24c46495c3
waleplanet <walexrealest@gmail.com>
2019-10-17 08:12:20 +0100

    init session store

--- helper/session.go
+++ helper/session.go
@@ -4,19 +4,66 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/boj/redistore"
+	"github.com/gomodule/redigo/redis"
 
 	"github.com/gorilla/sessions"
+)
+
+var (
+	Pool   *redis.Pool
+	Store  *redistore.RediStore
+	Domain string
+	//Secret         string
+	SessionTimeOut int
 )
 
 type AuthSession struct {
 	Session map[string]interface{}
 }
 
-func GetSession(rediStore *redistore.RediStore, r *http.Request, sessName, sessKey string) (*AuthSession, bool) {
+func newPool(server string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+func InitSessionStore(server, port, domain, secret string, timeout int) {
+	var err error
 
-	session, err := rediStore.Get(r, sessName)
+	if Store != nil {
+		return Store
+	}
+	Pool = newPool(fmt.Sprintf("%s:%s", server, port))
+	SessionTimeOut = timeout
+	Domain = Domain
+	Store, err = redistore.NewRediStoreWithPool(Pool, []byte(secret))
+	if err != nil {
+		panic(err)
+	}
+	return Store
+}
+
+func GetSession(r *http.Request, sessName, sessKey string) (*AuthSession, bool) {
+
+	if Store != nil {
+		log.Fatal(fmt.Errorf("redis: redistore is null"))
+	}
+
+	session, err := Store.Get(r, sessName)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -26,7 +73,7 @@ func GetSession(rediStore *redistore.RediStore, r *http.Request, sessName, sessK
 	obj, ok := session.Values[sessKey].(*AuthSession)
 	return obj, ok
 }
-func SaveSession(rediStore *redistore.RediStore, r *http.Request, w http.ResponseWriter, domain, sessName, sessKey string, sessionTimeout int, sessObj interface{}) error {
+func SaveSession(r *http.Request, w http.ResponseWriter, sessName, sessKey string, sessObj interface{}) error {
 	session, err := rediStore.Get(r, sessName)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -34,9 +81,9 @@ func SaveSession(rediStore *redistore.RediStore, r *http.Request, w http.Respons
 
 	session.Values[sessKey] = sessObj
 	session.Options = &sessions.Options{
-		Domain:   domain,
+		Domain:   Domain,
 		Path:     "/",
-		MaxAge:   sessionTimeout,
+		MaxAge:   SessionTimeOut,
 		HttpOnly: true,
 	}
 
