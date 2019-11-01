560f780f3abedd996ce8f5eaa47a36bce7c534ca
waleplanet <walexrealest@gmail.com>
2019-11-01 11:03:59 +0100

    add password reset token

--- events/contracts.go
+++ events/contracts.go
@@ -7,6 +7,13 @@ type UserCreatedEvent struct {
 	Host  string `json:"host"`
 }
 
+type PasswordReset struct {
+	ID    string `json:"id"`
+	Email string `json:"email""`
+	Token string `json:"token"`
+	Host  string `json:"host"`
+}
+
 func (e *UserCreatedEvent) EventName() string {
 	return "user.created"
 }
