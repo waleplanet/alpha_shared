0a241a2ba73acc07641c21d91f78ca1e03b0c5d1
waleplanet <walexrealest@gmail.com>
2020-01-11 14:48:24 +0100

    add generate dynamic random numerical string function:

--- helper/rand.go
+++ helper/rand.go
@@ -16,11 +16,21 @@ var seedRand *rand.Rand = rand.New(
 const charset = "abcdefghijklmnopqrstuvwxyz" +
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+const intset = "0123456789"
+
 func RandStringWithCharSet(length int, charset string) string {
 
 	b := make([]byte, length)
 	for i := range b {
 		b[i] = charset[seedRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandInt(length int, intset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = intset[seedRand.Intn(len(charset))]
 	}
 	return string(b)
 }
