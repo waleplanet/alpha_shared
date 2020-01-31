02c44e331e79b9b5955bb7cf359544acefb696df
waleplanet <walexrealest@gmail.com>
2020-01-31 15:12:53 +0100

    add username field to  welcome user eventi message

--- events/contracts.go
+++ events/contracts.go
@@ -2,26 +2,27 @@ package events
 
 type UserCreatedEvent struct {
 	ID    string `json:"id"`
-	Email string `json:"email""`
+	Email string `json:"email"`
 	Token string `json:"token"`
 	Host  string `json:"host"`
 }
 type WelcomeUserEvent struct {
-	ID    string `json:"id"`
-	Email string `json:"email""`
-	Host  string `json:"host"`
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Host     string `json:"host"`
+	Username string `json:"username"`
 }
 
 type PasswordReset struct {
 	ID    string `json:"id"`
-	Email string `json:"email""`
+	Email string `json:"email"`
 	Token string `json:"token"`
 	Host  string `json:"host"`
 }
 
 type OTPCreated struct {
 	ID    string `json:"id"`
-	Email string `json:"email""`
+	Email string `json:"email"`
 	Token string `json:"token"`
 	Host  string `json:"host"`
 }
