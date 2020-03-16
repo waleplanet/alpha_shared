aaee846611cc93325b18c669fb6e1691966ba7ff
waleplanet <walexrealest@gmail.com>
2020-03-16 21:23:26 +0100

    change Wallet model amount and ledger balance float64 to string

--- models/model.go
+++ models/model.go
@@ -6,15 +6,15 @@ type Wallet struct {
 	//"id": 138,
 	gorm.Model
 
-	UserId         string   `json:"user_id" gorm:"not null"`
-	CustomerID     int64    `json:"customer_id" gorm:"size:255;not null;unique"`
-	AvailableBal   *float64 `json:"available_balance"`
-	LedgerBal      *float64 `json:"ledger_balance"`
-	AccountType    string   `gorm:"type:varchar(1);not null" json:"account_type"`
-	WalletID       int64    `gorm:"size:255;not null" json:"id"`
-	WalletNo       string   `gorm:"size:255;not null" json:"wallet_number"`
-	Currency       string   `gorm:"type:varchar(20);not null" json:"currency"`
-	Status         string   `json:"status"`
-	DateCreated    string   `json:"date_created"`
-	DateBalUpdated string   `json:"date_balance_updated"`
+	UserId         string `json:"user_id" gorm:"not null"`
+	CustomerID     int64  `json:"customer_id" gorm:"size:255;not null;unique"`
+	AvailableBal   string `json:"available_balance"`
+	LedgerBal      string `json:"ledger_balance"`
+	AccountType    string `gorm:"type:varchar(1);not null" json:"account_type"`
+	WalletID       int64  `gorm:"size:255;not null" json:"id"`
+	WalletNo       string `gorm:"size:255;not null" json:"wallet_number"`
+	Currency       string `gorm:"type:varchar(20);not null" json:"currency"`
+	Status         string `json:"status"`
+	DateCreated    string `json:"date_created"`
+	DateBalUpdated string `json:"date_balance_updated"`
 }
